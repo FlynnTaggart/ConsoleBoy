b1d667ed478477e88a5f9bcc0399ef30f3974ce5
Mikhail <mr1302@ya.ru>
2021-12-05 01:02:44 +0300

    Made main page

--- js/article_list.js
+++ js/article_list.js
@@ -1,5 +1,11 @@
+// From backend
+
 let article_list = [
     ["Xbox", [
+        "articles/xbox/xbox.html",
+        "articles/xbox/xbox-game-studios.html"
+    ]],
+    ["Sony", [
         "articles/xbox/xbox.html",
         "articles/xbox/xbox-game-studios.html"
     ]]
--- js/article_picker.js
+++ js/article_picker.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     console.log(article_list);
-    for(var i = 0; i < article_list.length; ++i){
+    for(var i = 0; i < article_list.length; i++){
         var article_block = document.createElement("div");
         article_block.setAttribute("class", "titled_block");
         article_block.innerHTML = document.getElementById("block_to_create").innerHTML;
@@ -8,7 +8,7 @@ window.onload = function () {
         var id = article_list[i][0].replace(/\s/g, '_');
         article_block.innerHTML = article_block.innerHTML.replace(/{BLOCKID}/g, id);
         document.getElementsByClassName("articles_container")[0].appendChild(article_block);
-        for(var j = 0; j < article_list[i][1].length; ++j){
+        for(var j = 0; j < article_list[i][1].length; j++){
             var path = article_list[i][1][j];
             get_information(path, function(text) {
                 var div = document.createElement("div");
@@ -31,7 +31,7 @@ window.onload = function () {
 
 function get_information(link, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", link, true);
+    xhr.open("GET", link, false);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             callback(xhr.response);
